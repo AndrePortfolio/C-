1b954da41dea80479855c713bbd4c70c9c385e7f
Andre Alexandre Da Silva Piedade De Albuquerque <andre-da@c2r6s3.42lisboa.com>
2024-09-16 18:21:24 +0100

    update libft's

--- cpp_03/ex01/includes/libft.h
+++ cpp_03/ex01/includes/libft.h
@@ -17,15 +17,6 @@
 # include <string>			// For string class and functions
 # include <cstring>			// For C-style string functions
 # include <cmath>			// For common mathematical functions
-# include <vector>			// For the vector container
-# include <algorithm>		// For standard algorithms (sort, find, etc.)
-# include <map>				// For the map container
-# include <set>				// For the set container
-# include <queue>			// For the queue container
-# include <stack>			// For the stack container
-# include <list>			// For the list container
-# include <deque>			// For the deque container
-# include <iterator>		// For iterators
 # include <sstream>			// For string stream classes
 # include <fstream>			// For file stream classes
 # include <iomanip>			// For input/output manipulation
